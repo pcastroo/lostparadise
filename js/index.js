1d3b5c297f659b91575281576ecdeb3788987008
pcastroo <pedroddcastro21@gmail.com>
2020-10-04 21:41:25 -0300

    add navbar2/ add all products

--- js/index.js
+++ js/index.js
@@ -1,51 +1,92 @@
 const items=[
     {
         id:0,
-        name: 'Camiseta "DRAGON"',
-        price: 65,
+        name: 'Camiseta "DRAGON" preta',
+        price: 65.00,
         img: './src/bluedragonshirt.jpg',
         type: 'shirt',
+        description:"Camiseta preta , estampa silkscreen modelo unisexx, com dupla costura e cobre gola para maior resistencia.",
         quantities: 0
     },
     {
         id:1,
-        name: 'Camiseta "Global Smile"',
+        name: 'Camiseta "GLOBAL SMILE" preta',
         img: './src/globalsmileshirt.jpg',
-        price: 65,
+        price: 65.00,
         type: 'shirt',
+        description:"Camiseta preta , estampa silkscreen modelo unisexx, com dupla costura e cobre gola para maior resistencia.",
         quantities: 0
     },
     {
         id:2,
-        name: 'Moletom preto "LOSTPARADISE GANG"',
+        name: 'Moletom "FUTURISM" preto',
         img: './src/blackLPGsweatshirt.jpg',
-        price: 95,
+        price: 95.00,
         type: 'sweatshirt',
+        description:"Moletom lostparadise allblack, peça feita em algodão premium, flanelado e super quente para sua sessão com os amigos, modelo com logotipia FUTURISM NO PEITO.",
         quantities: 0
     },
     {
         id:3,
-        name: 'Moletom branco "FUTURISM"',
+        name: 'Moletom "FUTURISM" branco',
         img: './src/futurismsweatshirt.jpg',
-        price: 95,
+        price: 95.00,
         type: 'sweatshirt',
+        description:"Moletom LOSTPARADISE, peça feita em algodão premium, flanelado e super quente para sua sessão com os amigos, modelo com logotipia FUTURISM NO PEITO.",
         quantities: 0
     },
     {
         id:4,
-        name: 'Moletom branco "THE ROSES"',
+        name: 'Moletom "THE ROSES" branco',
         img: './src/whiteTRsweatshirt.jpg',
-        price: 95,
+        price: 95.00,
         type: 'sweatshirt',
+        description:"Moletom flanelado , estampa silkscreen modelo unisexx, com dupla costura e cobre gola para maior resistencia.",
         quantities: 0
-    }
+    },
+    {
+        id:5,
+        name: 'Camiseta "GLOBAL SMILE" branca',
+        img: './src/whiteglobalsmileshirt.jpg',
+        price: 65.00,
+        type: 'shirt',
+        description:"Camiseta branca , estampa silkscreen modelo unisexx, com dupla costura e cobre gola para maior resistencia.",
+        quantities: 0
+    },
+    {
+        id:6,
+        name: 'Moletom "DRAGON" preto',
+        price: 120.00,
+        img: './src/dragonsweatshirt.jpg',
+        type: 'sweatshirt',
+        description:"Moletom DRAGON, vem somente com 12 unidades para venda, a linha vem com estampa simples na frente e com o dragão maior nas costas, as peças vão ser disponibilizadas no dia 18/08, modelo sujeito a alteração de preço após o lançamento, preço para pre-venda.",
+        quantities: 0
+    },
+    {
+        id:7,
+        name: 'Manga longa "GLOBAL SMILE" cinza',
+        price: 79.90,
+        img: './src/greyglobalsmilelong.jpg',
+        type: 'long-sleeved',
+        description:"Manga longa cinza/mescla, estampa silkscreen modelo unisexx, com dupla costura e cobre gola para maior resistencia, tecido com lavagem de fabrica ",
+        quantities: 0
+    },
+    {
+        id:8,
+        name: 'Moletom "THE ROSES" preto',
+        img: './src/blackTRswearshirt.jpg',
+        price: 95.00,
+        type: 'sweatshirt',
+        description:"Moletom LOSTPARADISE, desenvolvido com o melhor algodão e costura, modelo perfeito para qualquer ocasião, momento, seu corte foi definido com qualquer padrão de estilo e de corpo, modelo vem também com cobre gola para uma maior resistência e acabamento, com forro no gorro do moletom, série vem com poucas unidades.",
+        quantities: 0
+    },
 ]
 
 function init(){
     var containerProducts = document.getElementById('products')
     items.map((val)=>{
         containerProducts.innerHTML += `
-        <div class="single-product">
+        <div class="single-product border border-dark rounded">
             <a href="#" key="`+val.id+`">
                 <img src="`+val.img+`">
                 <p>`+val.price+`</p>
